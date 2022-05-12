80525388105cb5e9de61b5b6dcabb238ccac865e
Solon Diego Garcia Moreira <sdiegogm@gmail.com>
2022-05-11 21:47:41 -0300

    Versão Final

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -1,11 +1,11 @@
 package com.dio.santander.bankline.api.controller;
 
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.service.CorrentistaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +16,16 @@ public class CorrentistaController {
     @Autowired
     private CorrentistaRepository correntistaRepository;
 
+    @Autowired
+    private CorrentistaService correntistaService;
+
     @GetMapping
     public List<Correntista> findAll(){
         return correntistaRepository.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody NovoCorrentista novoCorrentista){
+        correntistaService.save(novoCorrentista);
     }
 }
